79434a4ab3ef3fb8dd5da6fe223bfe8c5a27daf9
yilujun100 <yilujun100@qq.com>
2021-07-06 22:31:54 +0800

    feat: add climbing staris

---
+++ test/math/70.climbStairs.test.js
@@ -0,0 +1,9 @@
+import climbStairs from '../../code/math/70.climbStairs';
+
+test('climbing stairs', () => {
+	expect(climbStairs(2)).toBe(2);
+});
+
+test('climbing stairs 1:', () => {
+	expect(climbStairs(3)).toBe(3);
+});

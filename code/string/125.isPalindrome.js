986ccb9ffe99d44d7ec4102aa62512ff1c0dac32
yilujun100 <yilujun100@qq.com>
2022-03-09 10:04:04 +0800

    feat: add 125.isPalindrome

--- code/string/125.isPalindrome.js
+++ code/string/125.isPalindrome.js
@@ -4,6 +4,14 @@
  */
 
 export default s => {
-	const filteredS = s.toLowerCase().replace(/[^a-z0-9]/gi, '');
-	return filteredS.split('').reverse().join('') === filteredS;
+  const filteredS = s.toLowerCase().replace(/[^a-z0-9]/gi, '');
+  return filteredS.split('').reverse().join('') === filteredS;
+};
+
+const isPalindrome = s => {
+  s = s.toLowerCase().replace(/[^a-z0-9]/gi, '');
+  for (let i = 0, j = s.length - 1; i <= j; i++, j--) {
+    if (s.charAt(i) !== s.charAt(j)) return false;
+  }
+  return true;
 };

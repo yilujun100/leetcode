8dfa8d5570b1f81a716df7fe4f1fb99c218f663d
yilujun100 <yilujun100@qq.com>
2022-03-20 21:59:55 +0800

    add 101.对称二叉树 solution2

--- code/tree/101.symmetricTree.js
+++ code/tree/101.symmetricTree.js
@@ -16,3 +16,16 @@ function symmetricChecker(left, right) {
 
   return symmetricChecker(left.left, right.right) && symmetricChecker(left.right, right.left);
 }
+
+// solution2: 迭代
+const isSymmetric1 = root => {
+  const q = [root, root];
+  while (q.length) {
+    const [l, r] = [q.shift(), q.shift()];
+    if (!l && !r) continue;
+    if (!!l !== !!r || l.val !== r.val) return false;
+    q.push(l.left, r.right, l.right, r.left);
+  }
+
+  return true;
+};

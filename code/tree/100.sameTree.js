008c2a6ac4ea8ae10d3bfb138cfc5ed551dbb1cb
yilujun100 <yilujun100@qq.com>
2022-04-11 10:15:52 +0800

    feat: add 100.相同的树 comments

--- code/tree/100.sameTree.js
+++ code/tree/100.sameTree.js
@@ -4,3 +4,11 @@
 
 // 解题思路：
 // 递归判断即可。
+const isSameTree = (p, q) => {
+  // the same if both nodes are null
+  if (!p && !q) return true;
+  // not the same if either of the nodes is null or the values are different
+  if (!p || !q || p.val !== q.val) return false;
+
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};

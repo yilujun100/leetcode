31b4fca04056e43b4181fb5832f61fda15cf61dc
yilujun100 <yilujun100@qq.com>
2022-04-12 11:05:54 +0800

    feat: add 107.二叉树的层序遍历II solution2:recursive

--- code/tree/107.binaryTreeLevelOrderTraversalII.js
+++ code/tree/107.binaryTreeLevelOrderTraversalII.js
@@ -21,3 +21,24 @@ const levelOrderBottom = root => {
   }
   return result.reverse();
 };
+
+// solution2: recursive
+const levelOrderBottom1 = root => {
+  let h = height(root); // get the height using helper fn below
+  let result = [];
+  for (let depth = 1; depth <= h; depth++) {
+    result.push(levelOrder(root, depth));
+  }
+  return result.reverse();
+};
+
+function levelOrder(node, d, level = []) {
+  if (!node) return level;
+  if (d === 1) {
+    level.push(node.val);
+  } else if (d > 1) {
+    levelOrder(node.left, d - 1, level);
+    levelOrder(node.right, d - 1, level);
+  }
+  return level;
+}

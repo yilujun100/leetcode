df9bddf314a1807d64335324aeaf929d1b768be7
yilujun100 <yilujun100@qq.com>
2022-05-09 10:55:04 +0800

    feat: 501.二叉搜索树中的众数 solution2:inorder

--- code/tree/501.find-mode-in-binary-search-tree.js
+++ code/tree/501.find-mode-in-binary-search-tree.js
@@ -25,3 +25,31 @@ const findMode = function (root) {
   });
   return modes;
 };
+
+// solution2
+// 由于它是一个二叉搜索树，所以我们在进行中序遍历时可以将其视为升序排序的数组
+// 为了实现恒定空间(const space)，在遍历的过程中我们只需要存储当前节点信息(值和计数)并将当前节点值与前一个值进行比较：
+// 如果相等，更新当前值 count
+// 如果不相等，将当前计数重置为1
+const findMode1 = function (root) {
+  let mode = [],
+    curNodeVal = NaN,
+    curNodeCount = 0,
+    maxCount = -Infinity;
+
+  const inorder = function (root) {
+    if (!root) return;
+    inorder(root.left);
+    curNodeCount = (root.val === curNodeVal ? curNodeCount : 0) + 1;
+    curNodeVal = root.val;
+    if (curNodeCount > maxCount) {
+      mode = [root.val];
+      maxCount = curNodeCount;
+    } else if (curNodeCount === maxCount) {
+      mode.push(root.val);
+    }
+    inorder(root.right);
+  };
+  inorder(root);
+  return mode;
+};

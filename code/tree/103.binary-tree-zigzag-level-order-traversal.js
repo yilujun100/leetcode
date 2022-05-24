3d1d48aaad28599309e073ce344c49d6e008af6e
yilujun100 <yilujun100@qq.com>
2022-05-24 11:33:26 +0800

    feat: add 103.二叉树的锯齿形层序遍历 solution2:recursion

--- code/tree/103.binary-tree-zigzag-level-order-traversal.js
+++ code/tree/103.binary-tree-zigzag-level-order-traversal.js
@@ -27,3 +27,27 @@ const zigzagLevelOrder = function (root) {
 
   return output;
 };
+
+// solution2: recursion
+const zigzagLevelOrder1 = function (root) {
+  let res = [];
+  globalThis(root, 0, res);
+  return res;
+};
+function go(node, l, res) {
+  // l means level
+  if (!node) return;
+
+  if (res[l] == null) {
+    res.push([]);
+  }
+
+  if (l % 2 === 0) {
+    res[l].push(node.val);
+  } else {
+    res[l].unshift(node.val);
+  }
+
+  go(node.left, l + 1, res);
+  go(node.right, l + 1, res);
+}

b4951bf56ad809e81bf975d76a8eca467c1101a2
yilujun100 <yilujun100@qq.com>
2022-05-25 11:05:53 +0800

    feat: add 105.从前序与中序遍历序列构造二叉树

---
+++ code/tree/105.construct-binary-tree-from-preorder-and-inorder-traversal.js
@@ -0,0 +1,14 @@
+// 105.从前序与中序遍历序列构造二叉树
+// 题目大意
+// 根据一棵树的前序遍历与中序遍历构造二叉树。
+// 解题思路
+// 给出 2 个数组，根据 preorder 和 inorder 数组构造一棵树。
+// 利用递归思想，从 preorder 可以得到根节点，从 inorder 中得到左子树和右子树。只剩下一个节点的时候即为根节点。
+// 不断的递归直到所有的树都生成完成。
+const buildTree = function (preorder, inorder) {
+  const rootIndex = 0;
+  const rootValue = preOrder[rootIndex];
+  const pos = inorder.findIndex(value => value === rootValue);
+  const root = new TreeNode(rootValue);
+  root.left = buildTree();
+};

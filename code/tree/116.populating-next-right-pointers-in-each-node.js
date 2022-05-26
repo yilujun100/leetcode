9269230b53030dfa438fbb813316a7c9e1f4c100
yilujun100 <yilujun100@qq.com>
2022-05-26 11:16:52 +0800

    feat: add 116.填充每个节点的下一个右侧节点指针

---
+++ code/tree/116.populating-next-right-pointers-in-each-node.js
@@ -0,0 +1,19 @@
+// 116.填充每个节点的下一个右侧节点指针
+// 解题思路
+// 本质上是二叉树的层序遍历，基于广度优先搜索，将每层的节点放入队列，并遍历列进行连接。
+const connect = function (root) {
+  if (!root) return null;
+  let queue = [root];
+  while (queue.length) {
+    let len = queue.length;
+    for (let i = 0; i < len; i++) {
+      let node = queue.shift();
+      if (i !== len - 1) {
+        node.next = queue[0];
+      }
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+  }
+  return root;
+};

859aab49878c11aab09c6476cb8ef943b4d4c7be
yilujun100 <yilujun100@qq.com>
2022-06-13 10:53:00 +0800

    feat: 654.最大二叉树 solution2

--- code/tree/654.maximum-binary-tree.js
+++ code/tree/654.maximum-binary-tree.js
@@ -10,3 +10,16 @@ const constructMaximumBinaryTree = function (nums) {
   );
   return root;
 };
+
+// solution2
+const constructMaximumBinaryTree1 = (nums, low = 0, high = nums.length - 1) => {
+  if (low > high) return null;
+  let i = low;
+  for (let j = low + 1; j <= high; j++) {
+    if (nums[j] > nums[i]) i = j;
+  }
+  const root = new TreeNode(nums[i]);
+  root.left = constructMaximumBinaryTree1(nums, low, i - 1);
+  root.right = constructMaximumBinaryTree1(nums, i + 1, high);
+  return root;
+};

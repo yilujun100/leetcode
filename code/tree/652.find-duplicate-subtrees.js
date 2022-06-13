e7972ec78465aeca46d30da0f760b6ab94302117
yilujun100 <yilujun100@qq.com>
2022-06-13 10:16:10 +0800

    feat: add 652.寻找重复的子树

---
+++ code/tree/652.find-duplicate-subtrees.js
@@ -0,0 +1,20 @@
+// 652.寻找重复的子树
+// 解题思路
+// 序列化二叉树，使用深度优先搜索，其中递归函数返回当前子树的序列化结果。把每个节点开始的子树序列化结果保存在 map 中，然后判断是否存在
+// 重复的子树
+const findDuplicateSubtrees = root => {
+  const count = new Map();
+  const ans = [];
+  collect(root);
+  return ans;
+
+  function collect(node) {
+    if (!node) return '#';
+    let serial = `${node.val},${collect(node.left)},${collect(node.right)}`;
+    count.set(serial, count.has(serial) ? count.get(serial) + 1 : 1);
+    if (count.get(serial) === 2) {
+      ans.push(node);
+    }
+    return serial;
+  }
+};
